c4c0d0d905a8469626e415a3db82ff19f65ad128
Amanda Hay <amanda.hay@tufts.edu>
2012-05-24 21:48:14 -0400

    moved evernote SDK

--- EvernoteAnalytics/EvernoteAnalytics/account/models.py
+++ EvernoteAnalytics/EvernoteAnalytics/account/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="profile")
     evernote_token = models.CharField(blank=True, max_length=256)
     evernote_token_expires_time = models.DateTimeField(null=True, blank=True)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
