04a65f7c1fa95e64a26636b08fbdc7b7b0b5f7ef
Amanda Hay <amanda.hay@tufts.edu>
2012-05-31 17:47:37 -0400

    fixed css; prints out notebooks after authenticating

--- EvernoteAnalytics/settings.py
+++ EvernoteAnalytics/settings.py
@@ -63,11 +63,11 @@ MEDIA_URL = 'http://127.0.0.1:8000/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(ROOT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://127.0.0.1:8000/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
