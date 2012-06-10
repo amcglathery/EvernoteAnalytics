1924e14b08fe0aa48a8c8778c6e107762fd105c4
Matthew Russell <matthewjosephrussell@gmail.com>
2012-06-10 16:41:04 -0400

    Working on modularizing javascript and cleaning up html file for use with AJAX

---
+++ EvernoteAnalytics/static/js/googlePieChart.js
@@ -0,0 +1,20 @@
+//Requires jsapi to have been envoked beforehand and jquery
+function createPieChart(noteMapping, keyToDisplayMap, displayObjectName, displayElement){
+   google.load("visualization", "1", {packages:["corechart"]});
+   google.setOnLoadCallback(drawChart);
+   function drawChart() {
+     var data = new google.visualization.DataTable();
+     var ret = Array();
+     $.each(noteCount, function(i,obj){
+        ret.push([guidMap[i],obj]);
+     })
+     data.addColumn('string', displayObjectName);
+     data.addColumn('number', 'Numbers of Notes');
+     data.addRows(ret);
+     var options = {
+       title: 'Posts per ' + displayObjectName
+     };
+     var chart = new google.visualization.PieChart(document.getElementById(displayElement));
+     chart.draw(data, options);
+   }
+}
