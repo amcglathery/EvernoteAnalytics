5321ebadffa5860a3cdaccb3b542aad044def563
Andrew McGlathery <andrew.mcglathery@tufts.edu>
2012-06-16 17:30:05 -0400

    began work on auto-login with evernote, still having problems with logging in new users

--- EvernoteAnalytics/basic/views.py
+++ EvernoteAnalytics/basic/views.py
@@ -3,8 +3,13 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from evernote_auth import EvernoteAPI
 from analytics import EvernoteStatistics
+import thrift.protocol.TBinaryProtocol as TBinaryProtocol
+import thrift.transport.THttpClient as THttpClient
+import evernote.edam.userstore.UserStore as UserStore
 import logging
 import json
 
@@ -17,7 +22,7 @@ def run_evernote_auth(request):
         to request the user's token
     """
     callback_url = request.build_absolute_uri(reverse(
-        'basic.views.get_evernote_token', args=[]))
+        'basic.views.login_evernote_token', args=[]))
 
     everAuth = EvernoteAPI()
     return everAuth.get_token(request, callback_url)
@@ -26,7 +31,7 @@ def get_evernote_token(request):
     """ View that handles the callback from the Evernote OAuth call and
         stores the OAuth token for the user
     """
-    if request.user.is_authenticated:
+    if request.user.is_authenticated():
         everAuth = EvernoteAPI()
         credentials = everAuth.get_user_token(request)
         # credentials here contain OAuth token, save it!
@@ -40,12 +45,45 @@ def get_evernote_token(request):
         profile.evernote_token = credentials['oauth_token']
         profile.evernote_token_expires_time = expires_time
         profile.evernote_note_store_url = credentials['edam_noteStoreUrl']
-        evernoteHost = "sandbox.evernote.com"
-        userStoreUri = "https://" + evernoteHost + "/edam/user"
-
         profile.save()
     return HttpResponseRedirect(reverse('basic.views.post_evernote_js_token',
         args=[]))
+
+def login_evernote_token(request):
+    """ as get_evernote_token(), but logs the user in as well
+    """
+    everAuth = EvernoteAPI()
+    credentials = everAuth.get_user_token(request)
+    if request.user.is_authenticated():
+        user = request.user
+    else:
+        evernoteHost = "sandbox.evernote.com"
+        userStoreUri = "https://" + evernoteHost + "/edam/user"
+        userStoreHttpClient = THttpClient.THttpClient(userStoreUri)
+        userStoreProtocol = TBinaryProtocol.TBinaryProtocol(userStoreHttpClient)
+        userStore = UserStore.Client(userStoreProtocol)
+        evernoteUser = userStore.getUser(credentials['oauth_token'])
+        user = authenticate(user=evernoteUser.username, password=evernoteUser.id)
+        if not user:
+            newUser = User.objects.create_user(evernoteUser.username, evernoteUser.email, evernoteUser.id)
+            names = evernoteUser.name.split() if evernoteUser.name else None
+            newUser.first_name = names[0] if names and len(names) > 0 else ""
+            newUser.last_name = names[1] if names and len(names) > 1 else ""
+            newUser.save()
+            user = authenticate(user=evernoteUser.username, password=evernoteUser.id)
+        login(request, user)
+    try:
+        expires_time = datetime.fromtimestamp(int(credentials['expires']))
+    except TypeError:
+        logging.error("Error parsing token expires time")
+        expires_time = datetime.now()
+    user.profile.evernote_token = credentials['oauth_token']
+    user.profile.evernote_token_expires_time = expires_time
+    user.profile.evernote_note_store_url = credentials['edam_noteStoreUrl']
+    user.save()
+    return HttpResponseRedirect(reverse('basic.views.post_evernote_js_token',
+        args=[]))
+
 
 def post_evernote_token(request):
     """ View that is called after a user has sucessfully received a token
