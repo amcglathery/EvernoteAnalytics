aeca9a836c71592aad9d1cdbbde1602628b51616
Amanda Hay <amanda.hay@tufts.edu>
2012-06-30 19:30:36 -0400

    referral link

--- EvernoteAnalytics/account/views.py
+++ EvernoteAnalytics/account/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.conf import settings
 from django.template import RequestContext
 from urllib import urlencode
 
@@ -17,7 +18,8 @@ def login_page(request):
         next_page = reverse('basic.views.landing', args=[])
     if request.user.is_authenticated():
         return HttpResponseRedirect(next_page)
-    return render_to_response('login.html', { 'redirect_to': next_page},
+    return render_to_response('login.html', { 'redirect_to': next_page,
+        'evernote_key': settings.EVERNOTE_KEY},
         context_instance=RequestContext(request))
 
 def auth(request):
